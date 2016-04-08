4e533ac451227b73a6a60636bfcc62e47d7ad1dd
Kevin Go <gojohnkevin@gmail.com>
2016-04-08 18:34:12 +0800

    Added filters on admin

--- tundra/tundra/apps/pse/admin.py
+++ tundra/tundra/apps/pse/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from pse.models import Sector, Index
 
 
+class IndexAdmin(admin.ModelAdmin):
+    list_filter = ('status',)
+
 admin.site.register(Sector)
-admin.site.register(Index)
+admin.site.register(Index, IndexAdmin)
