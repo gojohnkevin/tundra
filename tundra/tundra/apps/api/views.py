2682909ce1910d1bc3177689c85a35911aa22bf9
Kevin Go <gojohnkevin@gmail.com>
2016-04-12 17:06:49 +0800

    Ticks

--- tundra/tundra/apps/api/views.py
+++ tundra/tundra/apps/api/views.py
@@ -3,12 +3,25 @@ from rest_framework import viewsets, filters
 from api.serializers import SectorSerializer, IndexSerializer
 from pse.models import Sector, Index
 
+import django_filters
+
+class SectorIndexFilter(django_filters.FilterSet):
+    created = django_filters.DateFilter(
+        name='index__created_at',
+    )
+
+    class Meta:
+        model = Sector
+        fields = ('name', 'created')
+
+
 
 class SectorViewSet(viewsets.ModelViewSet):
     queryset = Sector.objects.all().order_by('created_at')
     serializer_class = SectorSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
-    filter_fields = ('name',)
+    #filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = SectorIndexFilter
+    filter_fields = ('name', 'created',)
 
 
 class IndexViewSet(viewsets.ModelViewSet):
