30d1a0f6bae97a51dcf8f7afbd70a7f8a98dbc7c
Kevin Go <gojohnkevin@gmail.com>
2016-04-14 18:02:09 +0800

    Celery changes

--- tundra/tundra/celery.py
+++ tundra/tundra/celery.py
@@ -6,18 +6,13 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tundra.settings')
 
-from django.conf import settings  # noqa
+from django.conf import settings
 
 app = Celery('tundra')
 app.conf.update(
     CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend',
+    CELERY_TIMEZONE = 'Asia/Singapore'
 )
-app.conf.update(
-    CELERY_TIMEZONE = 'Asia/Singapore'   # set timezone in here
-)
-
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
